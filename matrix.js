e1f5fb5161193d1409936478e228aca4e614e593
Subrat Kumar Singh <85783015+subratcodes@users.noreply.github.com>
2022-10-31 06:32:13 +0000

    done matrix

--- matrix.js
+++ matrix.js
@@ -5,10 +5,61 @@ function sum(n,m,grid){
                sum+=Grid[i][j]
            }
        }
-       
        return sum
+}
+
+function multiply(a,b){
+
+
+  let rows=calculateRows(a)
+  let columns=calculateColumns(a)
+  
+
+ var rslt = Array(a.length).fill(0).map(()=>new 
+   Array(b.length).fill(0));
+
+
+  console.log(rslt)
+  
+}
+
+
+function diagonalSum(matrix){
 
   
+
+
+
+  
+}
+
+
+
+//calculates left diagnonal.
+function leftDiagnonal(matrix,n){
+  let sum=0
+  for(let i=0;i<n;i++){
+    for(let j=0;j<n;j++){
+      if(i==j){
+        sum+=matrix[i][j]
+      }
+    }
+  }
+
+  return sum
+}
+
+
+function rightDiagonal(matrix,n){
+  
+}
+
+function calculateRows(matrix){
+  return matrix[0].length
+}
+
+function calculateColumns(matrix){
+  return matrix.length
 }
 
 
